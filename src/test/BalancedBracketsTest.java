398c65091e4f7ae1c5f83ef4f7fb88e057b03cb4
Eka Darmawan <eka_darma@hotmail.com>
2022-08-05 22:16:15 -0400

    Finish Balanced Bracket Test

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,56 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void basicBalancedBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void outOfSequenceReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void bracketWithStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launchcode]"));
+    }
+
+    @Test
+    public void bracketBeforeStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Launchcode"));
+    }
+
+    @Test
+    public void bracketInsideStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Lau[nch]code"));
+    }
+
+    @Test
+    public void multipleBracketsInsideEachOther() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void multipleBracketsInsideEachOtherOutOfSequence() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][]["));
+    }
+
+    @Test
+    public void singleOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void multipleBracketPairsReturnsFalseOutofSequence() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]["));
     }
 
 
